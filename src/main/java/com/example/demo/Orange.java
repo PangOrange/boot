cb5c4e71e2b034bc001ac12632b5b9d6d602c7e8
PangOrange <993979434@qq.com>
2018-04-01 17:57:08 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/Orange.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Orange {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String name;
+
+	private double money;
+	
+	private Integer age;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getMoney() {
+		return money;
+	}
+	
+	public void setMoney(double money) {
+		this.money = money;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+}
